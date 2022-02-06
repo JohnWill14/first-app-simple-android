001f38bd77e3f2662f8aa63d5b6d2d96dcbe63f9
John William <johnwill.v.2017@gmail.com>
2022-02-06 20:55:19 -0300

    add first View

--- app/src/main/java/com/example/first_text/MainActivity.java
+++ app/src/main/java/com/example/first_text/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.first_text;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view){
+        EditText firstNameView = findViewById(R.id.editTextFirstName);
+        EditText secondNameView = findViewById(R.id.editTextSecondName);
+        EditText emailAddressView = findViewById(R.id.editTextTextEmailAddress);
+
+        TextView firstNameText = findViewById(R.id.textViewRespFirstName);
+        TextView secondNameText = findViewById(R.id.textViewRespSecondName);
+        TextView emailAddressText = findViewById(R.id.textViewRespEmail);
+
+        firstNameText.setText(firstNameView.getText());
+        secondNameText.setText(secondNameView.getText());
+        emailAddressText.setText(emailAddressView.getText());
     }
 }
